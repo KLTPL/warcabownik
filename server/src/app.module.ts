63f3fc28daa8051c15604449f1adddc0df90cf72
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-09-12 14:42:07 +0200

    feat(server): add game.module and game.dateway to listen for websocket connections with mocked ai move response
    The response move is mocked by

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,16 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { ConfigModule } from "@nestjs/config";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
+import { GatewayModule } from "./gateway/gateway.module";
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    GatewayModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
