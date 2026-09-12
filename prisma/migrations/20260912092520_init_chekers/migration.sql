cae4752032ba1aca201d8f89f2a25cdba02f8b71
KLTPL <105875079+KLTPL@users.noreply.github.com>
2026-09-12 11:34:04 +0200

    feat!: add new checkers prisma schema and migration

---
+++ prisma/migrations/20260912092520_init_chekers/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "GameStatus" AS ENUM ('IN_PROGRESS', 'FINISHED', 'DRAW', 'ABANDONED');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "whitePlayerId" TEXT,
+    "blackPlayerId" TEXT,
+    "status" "GameStatus" NOT NULL DEFAULT 'IN_PROGRESS',
+    "winnerId" TEXT,
+    "boardStateJson" TEXT NOT NULL DEFAULT '[]',
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Move" (
+    "id" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "turnNumber" INTEGER NOT NULL,
+    "fromPosition" TEXT NOT NULL,
+    "toPosition" TEXT NOT NULL,
+    "playerId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Move_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Move_gameId_turnNumber_key" ON "Move"("gameId", "turnNumber");
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_whitePlayerId_fkey" FOREIGN KEY ("whitePlayerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Game" ADD CONSTRAINT "Game_blackPlayerId_fkey" FOREIGN KEY ("blackPlayerId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Move" ADD CONSTRAINT "Move_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
